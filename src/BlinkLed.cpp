f40ee179706dc99a69850a73eaefd05047f9cddd
alexfigas <alexfigas11@gmail.com>
2022-03-23 10:55:55 +0000

    BlinkLed test

---
+++ src/BlinkLed.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+#define LED 2
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+}
--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   Serial.println("Hello World");
 }
